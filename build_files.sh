e8594b1a2bef08972b16dc2c484c417a80ff02a4
yashwanth2706 <yash2706.git@gmail.com>
2025-08-26 04:25:11 +0530

    configurec vercel.json and added print to build

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,11 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
+echo "=== Running build_files.sh ==="
 # Install Python dependencies
-python3 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Collect static files
-python3 manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
+
+echo "=== collectstatic finished ==="
